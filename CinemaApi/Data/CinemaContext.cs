dc8b2d3834521af7c0c83a4fe1c02ec3bbe77021
Robson <paivawork@hotmail.com>
2024-07-17 13:53:45 -0300

    Configuração do DBContext

---
+++ CinemaApi/Data/CinemaContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class CinemaContext : DbContext
+{
+    public CinemaContext(DbContextOptions<CinemaContext> options) : base(options) {}
+
+    public DbSet<Filme> Filmes { get; set; }
+    public DbSet<Sala> Salas { get; set; }
+}
