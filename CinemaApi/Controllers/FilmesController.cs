ab5dd2d710863ed5a02a969448da78bca065ad74
Robson <paivawork@hotmail.com>
2024-07-17 18:20:43 -0300

    Aplicando HTTP Codes

--- CinemaApi/Controllers/FilmesController.cs
+++ CinemaApi/Controllers/FilmesController.cs
@@ -24,9 +24,7 @@ public class FilmesController : ControllerBase
         var filme = await _context.Filmes.FindAsync(id);
 
         if (filme == null)
-        {
             return NotFound();
-        }
 
         return filme;
     }
@@ -38,5 +36,47 @@ public class FilmesController : ControllerBase
         await _context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetFilme), new { id = filme.Id }, filme);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutFilme(int id, Filme filme)
+    {
+        if (id != filme.Id)
+            return BadRequest();
+
+        _context.Entry(filme).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!FilmeExiste(id))
+                return NotFound();
+            else
+                throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFilme(int id)
+    {
+        var filme = await _context.Filmes.FindAsync(id);
+
+        if (filme == null)
+            return NotFound();
+
+        _context.Filmes.Remove(filme);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool FilmeExiste(int id)
+    {
+        return _context.Filmes.Any(e => e.Id == id);
     }
 }
--- CinemaApi/Controllers/SalasController.cs
+++ CinemaApi/Controllers/SalasController.cs
@@ -24,9 +24,7 @@ public class SalasController : ControllerBase
         var sala = await _context.Salas.FindAsync(id);
 
         if (sala == null)
-        {
             return NotFound();
-        }
 
         return sala;
     }
@@ -38,5 +36,49 @@ public class SalasController : ControllerBase
         await _context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetSala), new { id = sala.Id }, sala);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutSala(int id, Sala sala)
+    {
+        if (id != sala.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(sala).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch
+        {
+            if (!SalaExiste(id))
+                return NotFound();
+            else
+                throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteSala(int id)
+    {
+        var sala = await _context.Salas.FindAsync(id);
+
+        if (sala == null)
+            return NotFound();
+
+        _context.Salas.Remove(sala);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool SalaExiste(int id)
+    {
+        return _context.Salas.Any(e => e.Id == id);
     }
 }
