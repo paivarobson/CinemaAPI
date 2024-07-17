f0531cf0d33a73316ff7454658ff8ed899889de7
Robson <paivawork@hotmail.com>
2024-07-17 16:45:40 -0300

    Adicionado Diretor e Duração para Filme

--- CinemaApi/Models/Filme.cs
+++ CinemaApi/Models/Filme.cs
@@ -3,5 +3,7 @@ public class Filme
     public int Id { get; set; }
     public string Titulo { get; set; }
     public string Genero { get; set; }
-    public DateTime DataLancamento {get; set; }
+    public string Diretor { get; set; }
+    public int Duracao { get; set; }
+    public int AnoLancamento {get; set; }
 }
